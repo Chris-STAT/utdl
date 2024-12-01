5acecaecf75b43fcab0c4e248908c40d70403393
Chris-STAT <34548781+Chris-STAT@users.noreply.github.com>
2024-11-30 23:30:09 -0600

    update

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 HOMEWORK_DIR = Path(__file__).resolve().parent
 INPUT_MEAN = [0.2788, 0.2657, 0.2629]
